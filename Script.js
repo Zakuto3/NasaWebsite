21adb8046bc183ce30d9554609b7555dcc387159
Bimane1900 <S.roysson@bahnhof.se>
2018-09-16 21:29:35 +0200

    Scrolling animation

--- Script.js
+++ Script.js
@@ -1,15 +1,48 @@
-var index = 1;
+//var index = 1;
 
 function Slider() {
-	document.getElementById('slide-'+index).style.display = 'none';
-	if (index<3) {
-		document.getElementById('slide-'+(index+1)).style.display = 'block';
-		index++;
-	}
-	else{
-		index=1;
-		document.getElementById('slide-'+index).style.display = 'block';		
-	}
+	let cell = document.getElementsByClassName('size-4-cell');
+	cellContent = Array.from(cell);
+	cellContent.forEach(function(eles){
+		let children = Array.from(eles.children);
+		let showed = false;
+		children.forEach(function(curr){
+			console.log('curr', curr);
+			if(curr.classList.contains('slide-show')){
+				curr.classList.add('slide-close');
+				curr.classList.remove('slide-show');
+			}
+			else if(curr.classList.contains('slide-close')){
+				curr.classList.add('slide-closed');
+				curr.classList.remove('slide-close');
+				let moveToLast = curr.cloneNode(true);
+				curr.remove();
+				eles.appendChild(moveToLast);
+			}
+			else if(!showed){
+				curr.classList.remove('slide-closed');
+				curr.classList.add('slide-show');
+				showed = true;
+			}
+		});
+	});
+	// document.getElementById('slide-'+index).style.display = 'none';
+	// if (index<3) {
+	// 	document.getElementById('slide-'+(index+1)).style.display = 'block';
+	// 	index++;
+	// }
+	// else{
+	// 	index=1;
+	// 	document.getElementById('slide-'+index).style.display = 'block';		
+	// }
+	// if(el.classList.contains('slide-show')){
+	// 	el.classList.add('slide-close');
+	// 	el.classList.remove('slide-show');
+	// }
+	// else{
+	// 	el.classList.remove('slide-close');
+	// 	el.classList.add('slide-show');
+	// }
 }
 
-setInterval(Slider,1000);
+setInterval(Slider,2000);
