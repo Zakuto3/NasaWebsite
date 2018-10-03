2d321852535dd7106c0e7e1e2e4b4871cdadc6a9
Bimane1900 <34093582+Bimane1900@users.noreply.github.com>
2018-10-03 20:31:02 +0200

    Database and function for login

--- WebApplication2/LoginPage.aspx.cs
+++ WebApplication2/LoginPage.aspx.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MySql.Data.MySqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 namespace WebApplication2
 {
     public partial class LoginPage : System.Web.UI.Page
     {
+        string connectionString = "server=83.255.27.47;" +
+            "user id=Bimane;" +
+            "database=assignment3;" +
+            "port=3306;" +
+            "password=doggo21;" +
+            "pooling=true;";
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -24,8 +34,16 @@ namespace WebApplication2
 
         protected void loginBtn_Click(object sender, EventArgs e)
         {
-            Session["login"] = true;
-            Response.Redirect("Admin.aspx");
+            if (checkLogin(LoginName.Text, LoginPass.Text))
+            {
+                Session["login"] = true;
+                Response.Redirect("Admin.aspx");
+            }
+            else
+            {
+                Debug.WriteLine("Invalid password");
+                Session["login"] = false;
+            }
         }
 
         protected void adminBtn_Click(object sender, EventArgs e)
@@ -38,6 +56,29 @@ namespace WebApplication2
             Session["login"] = false;
             logoutBtn.Style.Add(HtmlTextWriterStyle.Display, "none");
             adminBtn.Text = "Log in";
+        }
+
+        protected bool checkLogin(string user, string pass)
+        {
+            string success;
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            MySqlCommand command = new MySqlCommand("CheckLogin", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("usr", user);
+            command.Parameters.AddWithValue("pwd", pass);
+
+            connection.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+            success = reader[0].ToString();
+            reader.Close();
+            connection.Close();
+            if (success == "0")
+            {
+                return true;
+            }
+            return false;
         }
 
     }
--- WebApplication2/Main.aspx.cs
+++ WebApplication2/Main.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MySql.Data.MySqlClient;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -14,7 +15,6 @@ namespace WebApplication2
 
        protected void Page_Load(object sender, EventArgs e)
        {
-
             if (Session["files"] != null)
             {
                 List<News> news = (List<News>)Session["files"];
